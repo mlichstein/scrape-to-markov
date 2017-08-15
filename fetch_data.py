064fd144aef229e6f3ae5613bb4033393e41d208
mlichstein <matt@lichsteins.com>
2017-08-15 17:46:17 -0400

    Use BeautifulSoup to find and clean lyrics more easily

--- fetch_data.py
+++ fetch_data.py
@@ -3,6 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 
 def fetch_lyrics(artist, song):
+  lyrics = ""
   artist = artist.lower()
   song = song.lower()
   artist = re.sub('[^A-Za-z0-9]+', "", artist)
@@ -10,17 +11,13 @@ def fetch_lyrics(artist, song):
   if artist.startswith("the"):
     artist = artist[3:]
   url = "https://azlyrics.com/lyrics/"+artist+"/"+song+".html"
-  header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.0; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0'}
+  header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.0;' \
+            ' WOW64; rv:24.0) Gecko/20100101 Firefox/24.0'}
 
   site = requests.get(url, headers=header)
   soup = BeautifulSoup(site.text, 'html.parser')
-  lyrics = str(soup)
-  top = '<!-- Usage of azlyrics.com content by any third-party lyrics provider is prohibited by our licensing agreement. Sorry about that. -->'
-  bottom = '<!-- MxM banner -->'
-  lyrics = lyrics.split(top)[1]
-  lyrics = lyrics.split(bottom)[0]
-  lyrics = lyrics.replace('<br>', '').replace('<br/>', '').replace('<i>', '').replace('</i>', '').replace('</div>', '')
-  lyrics = lyrics.strip()
+  for words in soup.find_all("div", {"class":""}):
+    lyrics += words.text
   return lyrics
 
-print fetch_lyrics("Foo Fighters", "My Hero")
+print fetch_lyrics("Foo Fighters", "Everlong")
