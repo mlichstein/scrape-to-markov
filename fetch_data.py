b7fb3b216e765b2733161174bbbd84bc15449e51
mlichstein <matt@lichsteins.com>
2017-08-16 10:20:52 -0400

    Use fake_useragent for header and initial work on run.py

--- fetch_data.py
+++ fetch_data.py
@@ -1,23 +1,22 @@
 import re
 import requests
+from fake_useragent import UserAgent
 from bs4 import BeautifulSoup
 
 def fetch_lyrics(artist, song):
   lyrics = ""
   artist = artist.lower()
   song = song.lower()
-  artist = re.sub('[^A-Za-z0-9]+', "", artist)
-  song = re.sub('[^A-Za-z0-9]+', "", song)
+  artist = re.sub('[^A-Za-z0-9]', "", artist)
+  song = re.sub('[^A-Za-z0-9]', "", song)
   if artist.startswith("the"):
     artist = artist[3:]
   url = "https://azlyrics.com/lyrics/"+artist+"/"+song+".html"
-  header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.0;' \
-            ' WOW64; rv:24.0) Gecko/20100101 Firefox/24.0'}
+  user_agent = UserAgent()
+  header = {'User-Agent': str(user_agent.random)}
 
   site = requests.get(url, headers=header)
   soup = BeautifulSoup(site.text, 'html.parser')
   for words in soup.find_all("div", {"class":""}):
     lyrics += words.text
   return lyrics
-
-print fetch_lyrics("Foo Fighters", "Everlong")
--- run.py
+++ run.py
@@ -1,2 +1,9 @@
-import fetch_data
+from fetch_data import fetch_lyrics
 from markov_python.cc_markov import MarkovChain
+
+def gather_info():
+  artist = str(raw_input("Enter the name of an artist or band: "))
+  song = str(raw_input("Enter a song title: "))
+  return fetch_lyrics(artist, song)
+
+print gather_info()
