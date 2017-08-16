d9188f5deb399c0c78cb1cce5026c29e091afe38
mlichstein <matt@lichsteins.com>
2017-08-16 10:39:11 -0400

    Move string lowering to input function

--- fetch_data.py
+++ fetch_data.py
@@ -5,8 +5,6 @@ from bs4 import BeautifulSoup
 
 def fetch_lyrics(artist, song):
   lyrics = ""
-  artist = artist.lower()
-  song = song.lower()
   artist = re.sub('[^A-Za-z0-9]', "", artist)
   song = re.sub('[^A-Za-z0-9]', "", song)
   if artist.startswith("the"):
@@ -14,7 +12,6 @@ def fetch_lyrics(artist, song):
   url = "https://azlyrics.com/lyrics/"+artist+"/"+song+".html"
   user_agent = UserAgent()
   header = {'User-Agent': str(user_agent.random)}
-
   site = requests.get(url, headers=header)
   soup = BeautifulSoup(site.text, 'html.parser')
   for words in soup.find_all("div", {"class":""}):
